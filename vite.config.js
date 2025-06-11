7d7fb414bdbe1b0b84374c4886ccb959aa017113
aneskar <udaigattu123@gmail.com>
2025-06-11 19:20:30 +0530

    Fix vite config and update package.json for deploy

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "/blogs", // 👈 use your repo name here
+// 👈 use your repo name here
   plugins: [react()],
 })
